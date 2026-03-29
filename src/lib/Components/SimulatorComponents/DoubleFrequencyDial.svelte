09e11b55a5eb2b20535af96191250a0016996159
Zac <zbenattar@gmail.com>
2026-03-29 12:02:58 +0100

    Fix build error

--- src/lib/Components/SimulatorComponents/DoubleFrequencyDial.svelte
+++ src/lib/Components/SimulatorComponents/DoubleFrequencyDial.svelte
@@ -1,5 +1,3 @@
-<!-- @migration-task Error while migrating Svelte code: `<button>` cannot be a child of `<button>`. The browser will 'repair' the HTML (by moving, removing, or inserting elements) which breaks Svelte's assumptions about the structure of your components.
-https://svelte.dev/e/node_invalid_placement -->
 <script lang="ts">
 	import { createEventDispatcher, onMount } from 'svelte';
 	export let DialEnabled: boolean = false; // Dial not interactive if disabled
@@ -97,7 +95,7 @@ https://svelte.dev/e/node_invalid_placement -->
 		intervalDuration = defaultIntervalDuration;
 	}
 
-	function updateIntervalDuration(incrementMethod: Function) {
+	function updateIntervalDuration(incrementMethod: () => void) {
 		if (intervalDuration > minIntervalDuration) {
 			clearInterval(intervalId);
 			intervalDuration -= intervalStep;
@@ -116,8 +114,12 @@ https://svelte.dev/e/node_invalid_placement -->
 			id={'frequency-center-div-' + id}
 			class="absolute"
 			style="top: 50%; left: 50%; position: absolute; margin: auto;"
-		/>
-		<button id={'double-frequency-dial-outer-' + id} class="double-frequency-dial-outer flex">
+		></div>
+		<button
+			id={'double-frequency-dial-outer-' + id}
+			class="double-frequency-dial-outer flex"
+			aria-label="Outer Dial"
+		>
 			<div class="absolute" style="left: 8px; top: 30%; width: 12px; pointer-events: none;">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.97 9.43"
 					><g opacity="0.25"
@@ -149,7 +151,6 @@ https://svelte.dev/e/node_invalid_placement -->
 				class="absolute flex flex-row"
 				style="top: 0px; left: 0px; width: 100%; height: 100%;"
 			>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<!-- svelte-ignore a11y-no-static-element-interactions -->
 				<div
 					class="relative"
@@ -158,8 +159,7 @@ https://svelte.dev/e/node_invalid_placement -->
 					on:mousedown={startIncrementingOuterAntiClockwiseHold}
 					on:mouseup={stopIncrementingOuterAntiClockwiseHold}
 					on:mouseleave={stopIncrementingOuterAntiClockwiseHold}
-				/>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				></div>
 				<!-- svelte-ignore a11y-no-static-element-interactions -->
 				<div
 					style="width: 50%;"
@@ -167,62 +167,61 @@ https://svelte.dev/e/node_invalid_placement -->
 					on:mousedown={startIncrementingOuterClockwiseHold}
 					on:mouseup={stopIncrementingOuterClockwiseHold}
 					on:mouseleave={stopIncrementingOuterClockwiseHold}
-				/>
+				></div>
 			</div>
-			<button
-				id={'double-frequency-dial-inner-' + id}
-				class="double-frequency-dial-inner absolute flex"
-			>
-				<div class="absolute" style="left: 8px; top: 26%; width: 12px; pointer-events: none;">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.7 5.92"
-						><g opacity="0.25"
-							><path
-								data-name="rad jog left in line"
-								d="M1.48 4.85a4.12 4.12 0 01-.81-2.46A4.06 4.06 0 011.26.26"
-								fill="none"
-								stroke="#fff"
-								stroke-miterlimit="10"
-							/><path data-name="rad jog left in arrow" fill="#fff" d="M2.7 3.23v2.69H0" /></g
-						></svg
-					>
-				</div>
-				<div class="absolute" style="right: 8px; top: 26%; width: 12px; pointer-events: none;">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.7 5.92"
-						><g opacity="0.25"
-							><path
-								data-name="rad jog right in line"
-								d="M1.57.26a4.07 4.07 0 01.6 2.13 4.13 4.13 0 01-.82 2.46"
-								fill="none"
-								stroke="#fff"
-								stroke-miterlimit="10"
-							/><path data-name="rad jog right in line" fill="#fff" d="M2.7 5.92H0V3.23" /></g
-						></svg
-					>
-				</div>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<div class="absolute flex flex-row" style="top: 0px; left: 0px; width:100%; height:100%;">
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<div
-						class="relative"
-						style="width: 50%;"
-						aria-label="Inner Dial Anti-Clockwise"
-						on:mousedown={startIncrementingInnerAntiClockwiseHold}
-						on:mouseup={stopIncrementingInnerAntiClockwiseHold}
-						on:mouseleave={stopIncrementingInnerAntiClockwiseHold}
-					/>
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<div
-						class="relative"
-						style="width: 50%;"
-						aria-label="Inner Dial Clockwise"
-						on:mousedown={startIncrementingInnerClockwiseHold}
-						on:mouseup={stopIncrementingInnerClockwiseHold}
-						on:mouseleave={stopIncrementingInnerClockwiseHold}
-					/>
-				</div></button
-			>
 		</button>
+		<button
+			id={'double-frequency-dial-inner-' + id}
+			class="double-frequency-dial-inner absolute flex"
+			aria-label="Inner Dial"
+		>
+			<div class="absolute" style="left: 8px; top: 26%; width: 12px; pointer-events: none;">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.7 5.92"
+					><g opacity="0.25"
+						><path
+							data-name="rad jog left in line"
+							d="M1.48 4.85a4.12 4.12 0 01-.81-2.46A4.06 4.06 0 011.26.26"
+							fill="none"
+							stroke="#fff"
+							stroke-miterlimit="10"
+						/><path data-name="rad jog left in arrow" fill="#fff" d="M2.7 3.23v2.69H0" /></g
+					></svg
+				>
+			</div>
+			<div class="absolute" style="right: 8px; top: 26%; width: 12px; pointer-events: none;">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.7 5.92"
+					><g opacity="0.25"
+						><path
+							data-name="rad jog right in line"
+							d="M1.57.26a4.07 4.07 0 01.6 2.13 4.13 4.13 0 01-.82 2.46"
+							fill="none"
+							stroke="#fff"
+							stroke-miterlimit="10"
+						/><path data-name="rad jog right in line" fill="#fff" d="M2.7 5.92H0V3.23" /></g
+					></svg
+				>
+			</div>
+			<div class="absolute flex flex-row" style="top: 0px; left: 0px; width:100%; height:100%;">
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					class="relative"
+					style="width: 50%;"
+					aria-label="Inner Dial Anti-Clockwise"
+					on:mousedown={startIncrementingInnerAntiClockwiseHold}
+					on:mouseup={stopIncrementingInnerAntiClockwiseHold}
+					on:mouseleave={stopIncrementingInnerAntiClockwiseHold}
+				></div>
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					class="relative"
+					style="width: 50%;"
+					aria-label="Inner Dial Clockwise"
+					on:mousedown={startIncrementingInnerClockwiseHold}
+					on:mouseup={stopIncrementingInnerClockwiseHold}
+					on:mouseleave={stopIncrementingInnerClockwiseHold}
+				></div>
+			</div></button
+		>
 	</div>
 </div>
 
